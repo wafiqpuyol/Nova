4ef22dd242151f1d81a6b63145b937712ae4abd6
wafiq <wafiqgithub775@gmail.com>
2024-12-28 07:40:41 +0600

    FIX: fix legacy-peer-deps & move comparePassword utility function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 import bcrypt from 'bcrypt';
 
 export const comparePassword = async (hashedPass: string, rawPass: string): Promise<{ errMsg: string, isPasswordMatched: boolean }> => {
